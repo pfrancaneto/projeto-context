1779703641a92e6f216d6eaf517e3d36db0fb373
Pedro França <pfranca.neto@gmail.com>
2022-09-29 18:31:54 -0300

    criando state + function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { Menu } from "./components/Menu";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToogleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Menu />
+      <Header handleToogleMenu={handleToogleMenu} />
+      <Menu showMenu={showMenu} />
     </div>
   );
 }
